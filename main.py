215d4f51e5d49f282daa37769714258c8131f895
M.Nassar <Nossa123##>
2022-03-30 21:21:39 +0200

    fix(main): Fix package name

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from translation_module import simple_html_translate, html_translate
+from htmlTranslator.translation_module import simple_html_translate, html_translate
 
 translated_html = simple_html_translate("<b>Hello</b>", source_language='en', target_language='es')
 translated_html_simple = html_translate("<YOUR HTML HERE>", source_language='en', target_language='es')
