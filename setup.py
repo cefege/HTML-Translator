8a76b7481410801d1c6bddae9594095f6f6cfdd9
M.Nassar <Nossa123##>
2022-03-30 21:18:23 +0200

    Refactoring

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("readme.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='html-translator',
+    name='htmlTranslator',
     version='0.0.1',
     author='Mihai Mateias',
     author_email='mateiasmihaiandrei@gmail.com',
     description='Package to translate the webpages content while keeping the same page structure',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=['html-translator'],
+    packages=['htmlTranslator'],
     install_requires=["beautifulsoup4==4.10.0", "deep-translator==1.8.1"],
 
 )
